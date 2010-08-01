7d1d9133e53f1dd82146c0183fe6705b31ff94e8
nifty <nifty02@gmail.com>
2010-08-01 15:37:06 +0200

    - template string function (should be moved) - started on item function

--- js/menu.js
+++ js/menu.js
@@ -3,22 +3,42 @@ jQuery(document).ready( function () {
 		
 	var Menu = function () {						
 		var container = $('<menu id="menuContainer"/>').appendTo('header'),
-			groupTemplate = '<li data-group-id="{id}" />';
+			groupTemplate = '<li data-group-id="{id}" />',
+			itemTemplate = '<button data-item-id="{id}" />';
+			
+		var templateStr = function (str, data) {
+			return str.replace(/\{([a-zA-Z1-9]+)\}/, function (match, tag) {
+				return data[tag] || '';
+			});
+		};
 		
+		templateStr(itemTemplate, {id:'test'});
 		return {
 			addMenuGroup: function (group_id) {
-				return container.append( groupTemplate );
-			},
-			addMenuItem: function(group_id) {
+				container.append( templateStr(groupTemplate, { id: group_id }) );
 				
+				return {
+					addMenuItem: function (item_id) {
+						return Menu.addMenuItem(group_id, item_id);
+					}
+				};
+			},
+			addMenuItem: function(group_id, item_id) {
+				var item = $(templateStr(itemTemplate, { id: item_id }));
+				
+				container.find('li[data-group-id="' + group_id + '"]').append( item );
+				
+				return item;
 			}
 		};
 	}();
 	
 	/*
 	 * Test */
-	 
-	Menu.addMenuGroup('test');
+	
+	var itemGroup = Menu.addMenuGroup('test');
+	itemGroup.addMenuItem('test2').text('test');
+	itemGroup.addMenuItem('test3').text('test2');
 	
 	Core.addModule('menu', Menu);
 });
