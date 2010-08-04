d04e84beb83d4ff902097e5ded4a90d7b0e36800
nifty <nifty02@gmail.com>
2010-08-04 15:12:15 +0200

    Modal window

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,10 @@ jQuery(document).ready( function () {
 	var Menu = function () {						
 		var _container,
 			_groupTemplate = '<li data-group-id="{id}" />',
-			_itemTemplate = '<button data-item-id="{id}" />';
+			_itemTemplate = '<button data-item-id="{id}" data-item-modal="{modal}" data-modal-window="{box}">{text}</button>';
 			
 		var templateStr = function (str, data) {
-			return str.replace(/\{([a-zA-Z1-9]+)\}/, function (match, tag) {
+			return str.replace(/\{([a-zA-Z1-9]+)\}/g, function (match, tag) {
 				return data[tag] || '';
 			});
 		};
@@ -15,6 +15,9 @@ jQuery(document).ready( function () {
 		return {
 			setContainer: function (container) {
 				_container = container;
+			},
+			getContainer: function () {
+				return _container;
 			},
 			addMenuGroup: function (group_id) {
 				if (typeof _container === 'undefined') {
@@ -24,17 +27,17 @@ jQuery(document).ready( function () {
 				_container.append( templateStr(_groupTemplate, { id: group_id }) );
 				
 				return {
-					addMenuItem: function (item_id) {
-						return Menu.addMenuItem(group_id, item_id);
+					addMenuItem: function (item_id, item_info) {
+						return Menu.addMenuItem(group_id, item_id, item_info);
 					}
 				};
 			},
-			addMenuItem: function(group_id, item_id) {
+			addMenuItem: function(group_id, item_id, item_info) {
 				if (typeof _container === 'undefined') {
 					throw new Error('Container item must be set');
 				}
-				
-				var item = $(templateStr(_itemTemplate, { id: item_id }));
+				item_info = item_info || {};
+				var item = $(templateStr(_itemTemplate, { id: item_id, text: item_info.text, modal: item_info.modal, box: item_info.box }));
 				
 				_container.find('li[data-group-id="' + group_id + '"]').append( item );
 				
