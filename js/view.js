700a39e060321724a5fbee9511c206fcc24ea9e1
nifty <nifty02@gmail.com>
2010-08-12 22:26:55 +0200

    started with views

---
+++ js/view.js
@@ -0,0 +1,24 @@
+(function () {
+	var Core = _budgetter,
+		db 	 = Core.getDB();
+		
+		
+	var templateStr = function (str, data) {
+		return str.replace(/\{([a-zA-Z1-9]+)\}/g, function (match, tag) {
+			return data[tag] || '';
+		});
+	};
+			
+	/*
+	 * API */
+	var View = function () {
+		var _activeView;
+		
+		return {
+
+		};
+	}();
+
+	
+	Core.addModule('view', View); 
+})(window);
