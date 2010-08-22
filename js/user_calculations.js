848cb244759ef2b76e91f0b112173b3d46a9bc38
nifty <nifty02@gmail.com>
2010-08-22 15:44:26 +0200

    Unsupported fallback

---
+++ js/user_calculations.js
@@ -0,0 +1,12 @@
+$(document).bind('ALL_MODULES_LOADED', function () {
+	var Budget = _budgetter.getModule('budget');
+	
+	
+	/*
+	 *	Use this file to add your own calculations
+	 *	See 'js/_core/calculations.js' for examples
+	 */
+	 
+	 
+	 
+});
